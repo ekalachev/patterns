8632f1efc4a9e188501d19452476a6b5d1caa687
Eldar Kalachev <rusgunter@gmail.com>
2019-10-02 12:14:54 +0300

    SOLID. Dependency Inversion Principle

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DesignPatterns.AbstractFactory;
 using DesignPatterns.Builder;
 using DesignPatterns.Prototype;
+using DesignPatterns.SOLID.DependencyInversion;
 using DesignPatterns.SOLID.LiskovSubstitution;
 using DesignPatterns.SOLID.OpenClosed;
 using DesignPatterns.TemplateMethod;
@@ -12,7 +13,23 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            SOLIDLiskovSubstitutuion();
+            SOLIDInterfaceSegregation();
+
+            Console.ReadLine();
+        }
+
+        static void SOLIDInterfaceSegregation()
+        {
+            var parent = new Person { Name = "John" };
+            var child1 = new Person { Name = "Chris" };
+            var child2 = new Person { Name = "Matt" };
+
+            // low-level module
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
+
+            new Research(relationships);
 
             Console.ReadLine();
         }
